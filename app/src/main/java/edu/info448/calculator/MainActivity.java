d5b0a4fe222383defd0bd64941934350f501b8b7
BraydonHall <BraydonH@cs.washington.edu>
2017-10-02 19:08:25 -0700

    Found all the bugs!!!! :-}

--- app/src/main/java/edu/info448/calculator/MainActivity.java
+++ app/src/main/java/edu/info448/calculator/MainActivity.java
@@ -54,7 +54,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 try {
-                    compute(Calculator.Operator.MUL);
+                    compute(Calculator.Operator.DIV);
                 } catch (IllegalArgumentException iae) {
                     Log.e(TAG, "IllegalArgumentException", iae);
                     mResultTextView.setText(getString(R.string.computationError));
@@ -96,7 +96,7 @@ public class MainActivity extends Activity {
                 result = String.valueOf(mCalculator.add(operand));
                 break;
             case SUB:
-                result = getString(R.string.computationError);
+                result = String.valueOf(mCalculator.sub(operand));
                 break;
             case DIV:
                 result = String.valueOf(mCalculator.div(operand));
